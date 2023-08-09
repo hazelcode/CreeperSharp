343228c7517769e1672c69c1e71544287e3ea4a0
SteveDeMaincra <lacosaqhabla@gmail.com>
2023-08-09 13:35:38 -0600

    XYZ Correction

--- Core/XYZ.cs
+++ Core/XYZ.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CreeperSharp.Core
+﻿namespace CreeperSharp.Core
 {
     public class XYZ
     {
         public string[] Set(string X, string Y, string Z)
         {
-            string[] Pos = { X, Y, Z};
+            string[] Pos = { X, Y, Z };
             return Pos;
         }
     }
