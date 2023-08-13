dd0437bfa42d40a92acac674bae2d1b08b22408e
Hazel Rojas <lacosaqhabla@gmail.com>
2023-08-12 23:32:52 -0600

    NEW: TextComponent and /tellraw

--- Core/Chat.cs
+++ Core/Chat.cs
@@ -1,10 +1,31 @@
-﻿namespace SteveSharp.Core
+﻿using SteveSharp.JsonShapes;
+using System.Text.Json;
+
+namespace SteveSharp.Core
 {
     public class Chat
     {
         public string Say(string msg)
         {
             return $"say {msg}";
+        }
+        public string Out(string selector, string msg, string? color = null, bool italic = false, bool bold = false, bool underlined = false, bool obfuscated = false, bool strikethrough = false, string? insertion = null, string? clickEventAction = null, string? clickEventValue = null)
+        {
+            var textComponent = new TextComponent
+            {
+                text = msg,
+                color = color,
+                italic = italic,
+                bold = bold,
+                underlined = underlined,
+                obfuscated = obfuscated,
+                strikethrough = strikethrough,
+                insertion = insertion,
+                clickEvent = new clickEvent { action = clickEventAction, value = clickEventValue }
+            };
+            
+            string command = "tellraw " + selector + " " + JsonSerializer.Serialize(textComponent);
+            return command;
         }
     }
 }
---
+++ JsonShapes/TextComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace SteveSharp.JsonShapes
+{
+    public class TextComponent
+    {
+        public string? text { get; set; }
+        public string? color { get; set; }
+        public bool italic { get; set; }
+        public bool bold { get; set; }
+        public bool underlined { get; set; }
+        public bool obfuscated { get; set; }
+        public bool strikethrough { get; set; }
+        public string? insertion { get; set; }
+        public clickEvent? clickEvent { get; set; }
+    }
+    public class clickEvent
+    {
+        public string? action { get; set; }
+        public string? value { get; set; }
+    }
+}
