f33ce0e52cc465b13aa4a22e0eadc4d2d1dec2b6
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2024-09-05 23:09:15 +0000

    Upgrade `For.Loop()` function

--- Core/For.cs
+++ Core/For.cs
@@ -2,11 +2,17 @@ namespace SteveSharp.Core;
 
 public static class For
 {
-    public static string Loop(int from, int to, string create, string replaces = ""){
-        string commands = "";
-        for(int i = from; i <= to; i++){
-            commands += create.Replace(replaces, i.ToString()) + '\n';
+    public static string Loop(int to, Func<int, List<String>> block, int from = 0) {
+        List<List<string>> fullBlock = new();
+        for(int i = from; i <= to; i++) {
+            fullBlock.Add(block(i));
         }
-        return commands;
+        List<string> ret = new();
+        foreach(var codeBlock in fullBlock) {
+            foreach(var command in codeBlock) {
+                ret.Add(command);
+            }
+        }
+        return string.Join("\n", ret);
     }
 }
