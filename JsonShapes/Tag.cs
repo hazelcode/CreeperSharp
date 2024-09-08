41b1bfccc3e290c1c1f0172a6f1d897cc5597698
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2024-09-08 22:48:53 +0000

    Ignore defaults and null fields when serializing TextComponents and Tags!

--- JsonShapes/Tag.cs
+++ JsonShapes/Tag.cs
@@ -1,8 +1,12 @@
-﻿namespace SteveSharp.JsonShapes
+﻿using System.Text.Json.Serialization;
+
+namespace SteveSharp.JsonShapes
 {
     public class Tag
     {
         public string[]? values { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool replace { get; set; }
     }
 }
--- JsonShapes/TextComponent.cs
+++ JsonShapes/TextComponent.cs
@@ -1,21 +1,35 @@
-﻿namespace SteveSharp.JsonShapes
+﻿using System.Text.Json.Serialization;
+
+namespace SteveSharp.JsonShapes
 {
     public class TextComponent
     {
         public string? text { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
         public string? color { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool italic { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool bold { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool underlined { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool obfuscated { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool strikethrough { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? insertion { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public clickEvent? clickEvent { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public TextComponent[]? extra { get; set; }
     }
     public class clickEvent
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? action { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? value { get; set; }
     }
 }
