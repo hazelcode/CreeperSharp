b3d7f7d26d115a774208d9fab46bf7de060c4f83
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2024-09-11 00:51:30 +0000

    Set `type` property name for `Type` property

--- JsonShapes/Recipes/CraftingDecoratedPot.cs
+++ JsonShapes/Recipes/CraftingDecoratedPot.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 
 public class CraftingDecoratedPot : CraftingSpecial {
+    [JsonPropertyName("type")]
     new public string Type { get; set; } = "minecraft:crafting_decorated_pot";
 }
