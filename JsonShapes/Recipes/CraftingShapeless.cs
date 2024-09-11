1a8f652a40220093597aa9cc8f713132d2babb32
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2024-09-11 01:49:03 +0000

    Add CraftingShapeless recipe

---
+++ JsonShapes/Recipes/CraftingShapeless.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace SteveSharp.JsonShapes.Recipes;
+
+public class CraftingShapeless : CraftingSpecial {
+    public class RecipeResult : Recipe.RecipeResult {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+    }
+
+    [JsonPropertyName("type")]
+    new public string Type { get; set; } = "minecraft:crafting_shapeless";
+
+    [JsonPropertyName("group")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Group { get; set; }
+    
+    [JsonPropertyName("ingredients")]
+    public dynamic[]? Ingredients { get; set; }
+
+    [JsonPropertyName("result")]
+    public RecipeResult? Result { get; set; }
+}
